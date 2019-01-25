63c07595512cdaeefd0a8d1bc190aa96933d11ac
Tobi Akerele <otakerele@gmail.com>
2019-01-25 03:51:46 -0500

    process case orders, add users to case queue

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-var cors = require('cors');
+var corsModule = require('cors');
 
-cors = cors({
+const cors = corsModule({
     origin: true
 });
 
@@ -11,8 +11,7 @@ const db = admin.database();
 
 const validateFirebaseIdToken = (req, res) => {
     return new Promise((resolve, reject) => {
-        if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
-            !req.cookies.__session) {
+        if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
             console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.');
             reject('Unauthorized');
         }
@@ -20,9 +19,9 @@ const validateFirebaseIdToken = (req, res) => {
         let idToken;
         if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
             idToken = req.headers.authorization.split('Bearer ')[1];
-        } else {
-            idToken = req.cookies.__session;
         }
+
+        if (!idToken) reject('Unauthorized');
         admin.auth().verifyIdToken(idToken)
             .then(decodedIdToken => {
                 resolve(decodedIdToken);
@@ -33,7 +32,6 @@ const validateFirebaseIdToken = (req, res) => {
     })
 };
 
-//
 exports.placeCaseOrder = functions.https.onRequest((req, res) => {
     if ('OPTIONS' == req.method && req.body == null) {
         res.status(204).send('Preflight was good');
@@ -44,31 +42,68 @@ exports.placeCaseOrder = functions.https.onRequest((req, res) => {
                 let userId = user['uid'];
                 // let bodyData = req.body;
 
-                addOrderToQueue(userId);
+                addOrderToQueue(userId).then(() => {
+                    res.status(200).send({
+                        type: 'success',
+                        message: 'Added order to queue'
+                    })
+                }).catch(error => {
+                    console.log(error);
+                    res.status(400).send({
+                        type: 'error',
+                        message: error
+                    });
+                });
             });
     });
 
     function addOrderToQueue(uid) {
-        db.ref(`userCarts/${uid}`).once('value')
+        const cartRef = db.ref(`userCarts/${uid}`);
+        return cartRef.once('value')
             .then(snapshot => {
                 if (!snapshot.exists()) return null;
 
                 // For each of the case ID, add it to the appropriate case queue
                 snapshot.forEach(snap => {
-                    console.log('Each cart id', snap.key);
                     // A nice way to always ensure that there are no duplicate or out of order orders.
                     db.ref(`caseQueues/${snap.key}`).transaction(caseData => {
-                        if (caseData) {
-                            if (!caseData.queue) caseData.queue = {};
-                            caseData.queue[uid] = true;
+                        if (!caseData) caseData = {};
 
-                            if (!caseData.queueCount) caseData.queueCount = 0;
-                            caseData.queueCount++;
-                        }
+                        if (!caseData.queue) caseData.queue = {};
+                        const position = Object.keys(caseData.queue).length;
+                        caseData.queue[position] = uid;
+
+                        if (!caseData.queueCount) caseData.queueCount = 0;
+                        caseData.queueCount++;
                         return caseData;
-                    })
+                    }).then(txData => {
+                        if (!txData.committed) throw new Error('Unable to add cases to queue.');
 
+                        return cartRef.remove();
+                    });
                 });
-            })
+            });
     }
 });
+
+exports.processCaseOrder = functions.database.ref('caseQueues/{caseId}/queue').onUpdate(async (snapshot, context) => {
+    const data = snapshot.after.val();
+    const caseId = context.params['caseId'];
+
+    // Get if the case is available
+    const isCaseAvailable = (await db.ref(`cases/${caseId}/isAvailable`).once('value')).val();
+
+    // If the case is available, work with the queue data object
+    if (isCaseAvailable && data) {
+
+        for (const index in data) {
+            if (!data.hasOwnProperty(index)) break;
+
+            const userId = data[index];
+
+
+        }
+    }
+});
+
+// TODO When a case is available, check the case's queue for if someone wants the device
