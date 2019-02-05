7eb39c39a795a1800abebd59314e49c76061f8a1
Tobi Akerele <otakerele@gmail.com>
2019-02-05 14:24:28 -0500

    create order, complete order, add user to queue

--- functions/index.js
+++ functions/index.js
@@ -42,7 +42,7 @@ exports.placeCaseOrder = functions.https.onRequest((req, res) => {
                 let userId = user['uid'];
                 // let bodyData = req.body;
 
-                addOrderToQueue(userId).then(() => {
+                createOrder(userId).then(() => {
                     res.status(200).send({
                         type: 'success',
                         message: 'Added order to queue'
@@ -56,54 +56,136 @@ exports.placeCaseOrder = functions.https.onRequest((req, res) => {
                 });
             });
     });
-
-    function addOrderToQueue(uid) {
-        const cartRef = db.ref(`userCarts/${uid}`);
-        return cartRef.once('value')
-            .then(snapshot => {
-                if (!snapshot.exists()) return null;
-
-                // For each of the case ID, add it to the appropriate case queue
-                snapshot.forEach(snap => {
-                    // A nice way to always ensure that there are no duplicate or out of order orders.
-                    db.ref(`caseQueues/${snap.key}`).transaction(caseData => {
-                        if (!caseData) caseData = {};
-
-                        if (!caseData.queue) caseData.queue = {};
-                        const position = Object.keys(caseData.queue).length;
-                        caseData.queue[position] = uid;
-
-                        if (!caseData.queueCount) caseData.queueCount = 0;
-                        caseData.queueCount++;
-                        return caseData;
-                    }).then(txData => {
-                        if (!txData.committed) throw new Error('Unable to add cases to queue.');
-
-                        return cartRef.remove();
-                    });
-                });
-            });
-    }
 });
 
+/**
+ * Anytime a new user is added to a case queue, check if the case in question is available. If it is, take the first
+ * user from the queue and push every user up one.
+ * @type {CloudFunction<Change<DataSnapshot>>}
+ */
 exports.processCaseOrder = functions.database.ref('caseQueues/{caseId}/queue').onUpdate(async (snapshot, context) => {
     const data = snapshot.after.val();
     const caseId = context.params['caseId'];
+    const parentRef =
 
     // Get if the case is available
     const isCaseAvailable = (await db.ref(`cases/${caseId}/isAvailable`).once('value')).val();
 
-    // If the case is available, work with the queue data object
+    // If the case is available and there is queue data
+    // , work with the queue data object
     if (isCaseAvailable && data) {
 
-        for (const index in data) {
-            if (!data.hasOwnProperty(index)) break;
+        if (!data.hasOwnProperty(1)) return;
 
-            const userId = data[index];
+        const userId = data[1];
+        completeOrder(userId, caseId)
+            .then(() => {
+                for (let i = 1; i < Object.keys(data).length; i++) {
+                    data[i] = data[i+1];
+                }
+            })
 
-
-        }
     }
 });
+
+
+/**
+ * This function creates an order for each case in a user's cart. If the case is presently available and there is
+ * no queue for the case, immediately process the user's order. If the case has a line for it or it is unavailable,
+ * create a queue and add the user to the bottom of the queue. First In First Out.
+ * @param uid
+ * @returns {Promise<admin.database.DataSnapshot | null>}
+ */
+function createOrder(uid) {
+    const cartRef = db.ref(`userCarts/${uid}`);
+
+    // Get the snapshot of the user's cart
+    return cartRef.once('value')
+        .then(snapshot => {
+            if (!snapshot.exists()) return null;
+
+            // For each of the case ID, do the required checks to determine how to get the user the case.
+            snapshot.forEach(async snap => {
+                const isCaseAvailable = (await db.ref(`cases/${snap.key}/isAvailable`).once('value')).val();
+
+                // If the case is available, complete the order for user without need for a queue.
+                if (isCaseAvailable) {
+                    return completeOrder(uid, snap.key);
+                }
+
+                queueUserOrder(uid, snap.key);
+            });
+        });
+}
+
+/**
+ * This function is triggered when the order has been completed. The order hasn't necessarily been fulfilled but it
+ * should be on its way there.
+ * @param uid
+ * @param caseId
+ */
+function completeOrder(uid, caseId) {
+    const orderRef = db.ref(`completeOrders/${caseId}`);
+    const pastOrderRef = db.ref(`userPastOrders/${uid}`);
+    const caseRef = db.ref(`cases/${caseId}`);
+    const userHistory = db.ref(`userHistory/${uid}`);
+    const timestamp = admin.database.ServerValue.TIMESTAMP;
+
+    // Add to complete order table, user's past orders and update the case availability.
+    return orderRef.push({
+        user: uid,
+        timestamp: timestamp
+    }).then(() => {
+        return pastOrderRef.push({
+            case: caseId,
+            timestamp: timestamp
+        })
+    }).then(() => caseRef.child('isAvailable').update(false))
+        .then(() => {
+            return userHistory.push({
+                timestamp: timestamp,
+                info: 'Order placed',
+                caseId: caseId
+            });
+        });
+}
+
+function queueUserOrder(uid, caseId) {
+    const userHistory = db.ref(`userHistory/${uid}`);
+    const cartRef = db.ref(`userCarts/${uid}`);
+
+    // A nice way to always ensure that there are no duplicate or out of order orders.
+    return db.ref(`caseQueues/${caseId}`).transaction(caseData => {
+        if (!caseData) caseData = {};
+
+        if (!caseData.queue) caseData.queue = {};
+        const position = Object.keys(caseData.queue).length;
+        caseData.queue[position + 1] = uid;
+
+        if (!caseData.queueCount) caseData.queueCount = 0;
+        caseData.queueCount++;
+        return caseData;
+    }).then(txData => {
+        if (!txData.committed) throw new Error('Unable to add cases to queue.');
+        const snap = txData.snapshot.val();
+        let info = 'Order Created! ';
+
+        if (snap.queue.uid > 1) {
+            info += `You are #${snap.queue.uid} in the queue`;
+        } else {
+            info += `Your order has been processed.`;
+        }
+
+        return userHistory.push({
+            timestamp: admin.database.ServerValue.TIMESTAMP,
+            info: info,
+            caseId: snap.key
+        });
+    }).then(() => {
+        return cartRef.remove();
+    }).catch(error => {
+        throw new Error(error);
+    });
+}
 
 // TODO When a case is available, check the case's queue for if someone wants the device
